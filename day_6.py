8a02e85fe60d3b424a4138ce4fa20d6d45582da9
griff9 <cgriffith09@gmail.com>
2022-12-05 22:25:35 -0700

    Create day_6.py

---
+++ day_6.py
@@ -0,0 +1,20 @@
+
+with open("input_6.txt", "r") as file:
+    data = file.read()
+
+def message_start(n) -> int:
+    markers = set()
+    for i,char in enumerate(data):
+        if char in markers:
+            markers = {char}
+            continue
+        markers.add(char)
+        if len(markers)==n:
+            return i + 1
+    return -1
+
+############################# Part 1
+print(f'Answer 1: {message_start(4)}')
+
+############################# Part 2
+print(f'Answer 2: {message_start(14)}')
