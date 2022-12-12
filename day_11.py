16ae3e7b234d674b65d72e6e9690d1b47a54a085
Cole Griffith <cgriffith09@gmail.com>
2022-12-11 18:22:20 -0700

    cleanup day 11

--- day_11.py
+++ day_11.py
@@ -1,43 +1,39 @@
-from collections import deque
 import heapq
 import math
 import re
 
 
 class Monkey:
-    def __init__(self, num, items, op, test, t_monkey, f_monkey) -> None:
-        self.num = num
-        self.items = [int(val) for val in items]
+    def __init__(self, monkey_info) -> None:
+        self.num = int(re.findall("\d", monkey_info[0])[0])
+        self.items = [int(i) for i in re.findall("\d+", monkey_info[1])]
+        self.op = monkey_info[2].split()[-2]
+        self.amount = monkey_info[2].split()[-1]
+        self.test = int(re.findall("\d+", monkey_info[3])[0])
+        self.t_monkey = int(re.findall("\d+", monkey_info[4])[0])
+        self.f_monkey = int(re.findall("\d+", monkey_info[5])[0])
         self.cnt = 0
-        self.op, self.amount = op.split()[-2], op.split()[-1]
-        self.test = test
-        self.t_monkey = t_monkey
-        self.f_monkey = f_monkey
 
 
-with open("inputs\input_test.txt", "r") as file:
+with open("inputs\input_11.txt", "r") as file:
     data = file.read()
-data = deque(data.split("\n"))
-monos = []
-
-while data:
-    num = int(re.findall("\d", data.popleft())[0])
-    items = re.findall("\d+", data.popleft())
-    op = data.popleft()
-    test = int(re.findall("\d+", data.popleft())[0])
-    t_monkey = int(re.findall("\d+", data.popleft())[0])
-    f_monkey = int(re.findall("\d+", data.popleft())[0])
-    monos.append(Monkey(num, items, op, test, t_monkey, f_monkey))
-    if data:
-        data.popleft()
+data = data.split("\n\n")
 
 
-def monkey_business(rounds, lcm) -> int:
-    for rnd in range(rounds):
+def create_monkeys() -> list:
+    monos = []
+    for monkey in data:
+        monos.append(Monkey(monkey.split("\n")))
+    return monos
+
+
+def monkey_business(monos, rounds, lcm=None) -> int:
+    for _ in range(rounds):
         for m in monos:
             for i in m.items:
                 amount = int(m.amount) if m.amount != "old" else i
-                i = (i * amount) % lcm if m.op == "*" else (i + amount) % lcm
+                i = (i * amount) if m.op == "*" else (i + amount)
+                i = i % lcm if lcm else i // 3
                 if i % m.test != 0:
                     monos[m.f_monkey].items.append(i)
                 else:
@@ -46,9 +42,9 @@ def monkey_business(rounds, lcm) -> int:
             m.items = []
 
     top_2 = heapq.nlargest(2, monos, key=lambda x: x.cnt)
-    #print(top_2[0].cnt,top_2[1].cnt)
     return top_2[0].cnt * top_2[1].cnt
 
-#I broke answer 1 somehow and need to fix it later
-#print(f'Answer 1: {monkey_business(20,3)}')
-print(f"Answer 2: {monkey_business(10000,math.lcm(3,5,2,13,11,17,19,7))}") #hard coded the divisibles as lcm was complaining about taking an iterable
+
+print(f"Answer 1: {monkey_business(create_monkeys(),20)}")
+print(f"Answer 2: {monkey_business(create_monkeys(),10000,math.lcm(3,5,2,13,11,17,19,7))}")
+# hard coded the divisibles as lcm was complaining about taking an iterable
